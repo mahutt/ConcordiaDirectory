2e2f91acb8ae1216fdc6bb625f4ebcd49f1a147a
mahutt <tommy.mahut@gmail.com>
2024-09-05 19:52:55 -0400

    Fix spacing on smaller screen sizes and add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,15 +18,15 @@ function App() {
   }, [searchQuery])
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-orange-600 to-red-700 text-white font-sans">
+    <div className="min-h-screen bg-gradient-to-br from-orange-600 to-red-700 text-white font-sans px-4 flex flex-col">
       <header className="container mx-auto pb-8 pt-32">
-        <h1 className="tracking-tight text-6xl font-bold text-center mb-2 bg-gradient-to-r from-slate-300 via-slate-100 to-slate-300 bg-clip-text text-transparent">
+        <h1 className="tracking-tight text-5xl font-bold text-center mb-2 pb-2 bg-gradient-to-r from-slate-300 via-slate-100 to-slate-300 bg-clip-text text-transparent">
           Concordia Directory
         </h1>
         <p className="text-xl text-center">Connecting Minds Across Campus</p>
       </header>
 
-      <main className="container mx-auto mt-12">
+      <main className="container mx-auto mt-12 flex-1">
         <div className="max-w-3xl mx-auto">
           <div className="relative">
             <input
@@ -45,6 +45,9 @@ function App() {
           {people.length == 0 && <Features focusInput={focusInput} />}
         </div>
       </main>
+      <footer className="mt-auto text-center opacity-50 py-4">
+        <p>Last updated: N/A</p>
+      </footer>
     </div>
   )
 }
