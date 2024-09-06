cc6243cfe1f96639654f5f51cefecfae243001fe
mahutt <tommy.mahut@gmail.com>
2024-09-06 13:27:26 -0400

    Change backend routing to /api

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
   const search = req.query.search
   if (!search) {
     res.json([])
